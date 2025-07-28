43301bc74855a334f445f883502cb3a01985fc18
sabribenradhia <sabri@houseofichigo.com>
2025-07-28 17:46:41 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({base: "/strat-ai-plan-84-main/",
+export default defineConfig(({ mode }) => ({
+  base: "/strat-ai-plan-84-main/",
   server: {
     host: "::",
     port: 8080,
