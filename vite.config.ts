7be2a444d8e2d4182129da0d8c551fc0e3797cdd
ahmed-oueslati <ahmed@houseofichigo.com>
2025-07-28 19:42:11 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/strat-ai-plan-84/",
+  base: "/",
   server: {
     host: "::",
     port: 8080,
